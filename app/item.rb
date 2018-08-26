9ad9f84d74ef3a0c527e5acb2567734a618d354f
Jason Viger <jason.viger@gmail.com>
2018-08-26 08:57:42 +0000

    automatically backed up by learn

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,15 @@
 class Item
   attr_accessor :name, :price
 
+  @@itmems = []
+  
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
+  end
+  
+  def self.all 
+    @@items 
   end
 end
