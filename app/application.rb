30fd71848304e033d5f8aabc9101c8886f604113
Jason Viger <jason.viger@gmail.com>
2018-08-26 08:42:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,9 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
+    
+    if req.path=="/songs"
+      resp.write "You requested the songs"
     else
       resp.write "Path Not Found"
       resp.status = 404
