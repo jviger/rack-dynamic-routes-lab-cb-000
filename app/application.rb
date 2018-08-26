454745644e0143665739b7564c4cda16b823034d
Jason Viger <jason.viger@gmail.com>
2018-08-26 08:47:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,8 @@ class Application
     
     if req.path.match(/songs/)
       item_name = req.path.split("/items/").last
-      item = @@songs.find{|i| i.name == item_name}
- 
-      resp.write song.artist
+      item = @@items.find{|i| i.name == item_name}
+      resp.write item.price
     else
       resp.write "Route not found"
       resp.status = 404
