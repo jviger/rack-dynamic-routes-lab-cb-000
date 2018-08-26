def458b4e7f1653c77aa6ea7e904d1ba6acee40a
Jason Viger <jason.viger@gmail.com>
2018-08-26 09:01:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path.match(/songs/)
+    if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
